4106b4c9a53fab6657d2f9d17f25597aae914a6d
TranGiaHung-B2014981 <aurarewop@gmail.com>
2025-12-16 18:04:27 +0700

    Upload code

---
+++ src/main/java/com/tghung30302/crud_employee_react_spring/CrudEmployeeReactSpringApplication.java
@@ -0,0 +1,13 @@
+package com.tghung30302.crud_employee_react_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudEmployeeReactSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudEmployeeReactSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tghung30302/crud_employee_react_spring/CrudEmployeeReactSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tghung30302.crud_employee_react_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudEmployeeReactSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
